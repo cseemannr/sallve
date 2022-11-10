299a35a75c60cd9f6e8fc30ac6601b60c15cf01f
csr <camilaseemannramos@gmail.com>
2022-11-10 22:29:26 +0100

    All Media Queries Done

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const headerLimeArray = [
   "frete grátis acima de R$109 😎",
   "descontudo com até 40% OFF 😍",
 ];
+const testimonials = document.querySelector(".testimonials").children;
+const brandTestimonials = document.querySelector(".brands").children;
 
 //change lime nav text
 let i = 0;
@@ -16,3 +18,21 @@ function changeText() {
 }
 changeText();
 setInterval(changeText, 5000);
+
+//testimonials
+console.log(testimonials);
+console.log(brandTestimonials);
+
+for (let i = 0; i < brandTestimonials.length; i++) {
+  brandTestimonials[i].addEventListener("click", function () {
+    for (let j = 0; j < brandTestimonials.length; j++) {
+      brandTestimonials[j].classList.remove("active");
+    }
+    brandTestimonials[i].classList.add("active");
+
+    for (let k = 0; k < testimonials.length; k++) {
+      testimonials[k].classList.remove("active");
+    }
+    testimonials[i].classList.add("active");
+  });
+}
